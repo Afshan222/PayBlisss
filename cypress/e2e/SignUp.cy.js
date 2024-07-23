1febfa6bfb497c06b500419c253c9b76314285b4
afshan <afshanimtiaz111@gmail.com>
2024-07-23 12:59:07 +0500

    Initial

---
+++ cypress/e2e/SignUp.cy.js
@@ -0,0 +1,14 @@
+import SignUp from "../support/POM/SignUpPO";
+
+const SignUpData = new SignUp()
+describe('Payblisss', function() {
+    before(() => {
+        cy.visit('https://business.payblisss.com/signup', { failOnStatusCode: false });
+        cy.fixture(Cypress.env('datafile')).then((data) => {
+            this.data = data;
+        })
+    });
+    it('SignUp', function() {
+        SignUpData.BusinessNameData(this.data) 
+    });
+});
